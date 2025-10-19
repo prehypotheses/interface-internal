d8f98cf3ae5229bda216a759bd1d2e7b4baab219
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-19 16:03:55 +0100

    Update src/clients/initial.py

--- src/clients/initial.py
+++ src/clients/initial.py
@@ -17,18 +17,19 @@ class Initial:
     because the underlying software allows for extensive customisation.
     """
 
-    def __init__(self):
+    def __init__(self, arguments: dict):
         """
 
-        Constructor
+        :param arguments: A set of arguments vis-à-vis calculation, interface, and storage objectives.
         """
 
         self.__configurations = config.Config()
 
         # Pipeline
+        # noinspection PyTypeChecker
         self.__classifier = transformers.pipeline(
             task='ner', model=os.path.join(self.__configurations.data_, 'model'),
-            device=self.__configurations.device)
+            device=arguments.get('device'))
 
     def __custom(self, paragraph):
         """
